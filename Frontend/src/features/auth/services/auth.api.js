e309f078967fd57cca9a94dd7b00f2449cbca840
Pritam Kumar <pritampriyam07@gmail.com>
2026-03-24 00:01:22 +0530

    login bugs fixed

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,8 +2,8 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
-    // baseURL: "https://interviewnavigatorbackend.onrender.com",
+   // baseURL: "http://localhost:3000",
+    baseURL: "https://interviewnavigatorbackend.onrender.com",
     withCredentials: true
 })
 
